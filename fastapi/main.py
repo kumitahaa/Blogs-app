bd5a648ff79745d1b60b9797bd2030eeca6a8e9e
Kumitahaa <kumitaha123@gmail.com>
2024-02-04 13:26:45 +0500

    Setup React Project - maintained files

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,8 +3,24 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import blog,user,authentication
+from fastapi.middleware.cors import CORSMiddleware
 # ---------------------------------------- Create Instance -----------------------------------------------
 app = FastAPI() 
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3000/",
+    "https://localhost:3000/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(authentication.router)
 app.include_router(blog.router)
